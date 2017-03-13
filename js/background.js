bbee22ff4b0e26f40be06a554674447e8658b47e
Tyler Yep <tyep10@gmail.com>
2017-03-13 14:52:02 -0700

    Fix Drag and Drop

--- js/background.js
+++ js/background.js
@@ -9,5 +9,16 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    switch(ev.target.id) {
+    case "drag1":
+        break;
+    case "drag2":
+        break;
+    case "drag3":
+        break;
+    case "drag4":
+        break;
+    default:
+        ev.target.appendChild(document.getElementById(data));
+    }
 }
